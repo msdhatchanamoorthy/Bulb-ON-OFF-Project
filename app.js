1f6b433c397456734bfad20d0533040553049850
Dhatchanamoorthy M S <msdhatchanamoorthy001@gmail.com>
2025-03-19 12:21:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_btn = document.getElementById("on");
+var off_btn = document.getElementById("off");
+var img = document.getElementById("bulb");
+
+function bulb_on(){
+    img.src = "https://cdn.pixabay.com/photo/2013/07/12/14/34/light-148483_1280.png"
+}
+
+function bulb_off(){
+    img.src = "https://cdn.pixabay.com/photo/2013/07/12/14/34/filament-148482_1280.png"
+}
